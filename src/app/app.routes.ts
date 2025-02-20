6ce32e8c8e5473fe1b1da0da2ad9ffd1d3045bca
DayanaCueva18 <dannacorrea665@gmail.com>
2025-02-20 11:47:26 -0500

    enviar cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path:'index',loadComponent:()=>import('./index/index.component')},
+  {path:'index',loadComponent:()=>import('./index/index.component')}, 
+  {path:'login',loadComponent:()=>import('./login/login.component')},
   {path:'**',
     redirectTo:'index'
   },
--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
