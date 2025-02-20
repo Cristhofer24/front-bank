2eb5ae42eecb95205e9b53a74d9acd63ecd3e623
cristhofer24 <you@example.com>
2025-02-20 11:28:07 -0500

    Agregacion de talwin y pagina index

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'index',loadComponent:()=>import('./index/index.component')},
+  {path:'**',
+    redirectTo:'index'
+  },
+
+];
