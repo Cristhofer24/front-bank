a54a5f707ba9a4755166820b1ac0fdc0b9d7248d
Santos-de-Israel <144865373+Santos-de-Israel@users.noreply.github.com>
2025-03-01 22:39:13 -0500

    cambios Css

--- src/app/Pages/abrir-cuenta/abrir-cuenta.component.ts
+++ src/app/Pages/abrir-cuenta/abrir-cuenta.component.ts
@@ -2,10 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-abrir-cuenta',
-  imports: [],
   templateUrl: './abrir-cuenta.component.html',
-  styleUrl: './abrir-cuenta.component.css'
+  styleUrls: ['./abrir-cuenta.component.css']
 })
 export default class AbrirCuentaComponent {
+  ngOnInit() {
+    const clienteIdInput = document.querySelector("input[name='clienteId']") as HTMLInputElement;
+    if (clienteIdInput && clienteIdInput.value) {
+      localStorage.setItem('clienteId', clienteIdInput.value);
+    }
 
+    const storedClienteId = localStorage.getItem('clienteId');
+    if (storedClienteId) {
+      clienteIdInput.value = storedClienteId;
+    }
+  }
 }
