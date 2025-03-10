3c6df50ee8292b640e706b766628c47ceea0b51d
cristhofer24 <you@example.com>
2025-03-10 17:04:39 -0500

    Enviar cuentas al back-end

--- src/app/Services/cuenta.service.ts
+++ src/app/Services/cuenta.service.ts
@@ -1,18 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Cuenta } from '../Interfaces/Cuenta';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CuentaService {
-  private ApiUrl='http://localhost:8080/api/Cuentas';
 
-  constructor(private http:HttpClient) { }
+  private ApiUrl = 'http://localhost:8080/api/cuentas';
 
-//Guardar Clientes
-createCuenta(cuentas: Cuenta): Observable<Cuenta> {
-  return this.http.post<Cuenta>(this.ApiUrl, cuentas);
-}
+  constructor(private http: HttpClient) {}
+
+  //Guardar Clientes
+  createCuenta(cuentas: any): Observable<any> {
+    return this.http.post<Cuenta>(this.ApiUrl, cuentas);
+  }
+
+  generarCuenta(tipoCuenta: string): Observable<any> {
+    const params = new HttpParams().set('tipoCuenta', tipoCuenta);
+    return this.http.get<any>(`${this.ApiUrl}/ObtenerValores`, { params });
+  }
 }
